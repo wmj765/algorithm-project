7ae5eac96d5aa6423a175dd83f693bd10d33785c
maojia <Maojia.Wang@lingyue-digital.com>
2020-04-17 18:42:17 +0800

    add chaper02 readme

---
+++ src/main/java/chapter01/Practice.java
@@ -0,0 +1,44 @@
+package chapter01;
+
+/**
+ * @author wangmj
+ **/
+public class Practice {
+
+    //最好时间复杂度O(1)
+    //最坏时间复杂度O(n)
+    //平均时间复杂度O(1)
+    //均摊时间复杂度O(1)
+
+
+    // 全局变量，大小为10的数组array，长度len，下标i。
+    int array[] = new int[10];
+    int len = 10;
+    int i = 0;
+
+    // 往数组中添加一个元素
+    void add(int element) {
+        if (i >= len) { // 数组空间不够了
+            // 重新申请一个2倍大小的数组空间
+            int new_array[] = new int[len * 2];
+            // 把原来array数组中的数据依次copy到new_array
+            for (int j = 0; j < len; ++j) {
+                new_array[j] = array[j];
+            }
+            // new_array复制给array，array现在大小就是2倍len了
+            array = new_array;
+            len = 2 * len;
+        }
+        // 将element放到下标为i的位置，下标i加一
+        array[i] = element;
+        ++i;
+
+        int arr[][] = new int[5][6];
+        arr[0][0] = 1;
+
+
+    }
+
+
+
+}
