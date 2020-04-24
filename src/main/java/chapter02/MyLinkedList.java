34aceb88e9fe042092a96c2fc6db7498c0274246
maojia <Maojia.Wang@lingyue-digital.com>
2020-04-24 23:41:23 +0800

    add reverse linkedlist

--- src/main/java/chapter02/MyLinkedList.java
+++ src/main/java/chapter02/MyLinkedList.java
@@ -18,21 +18,20 @@ public class MyLinkedList<T> {
 
     public void add(T data) {
         if (head == null) {
-            head = new Node(data,null);
+            head = new Node(data, null);
         } else {
             Node n = head;
             while (n.next != null) {
-                n=n.next;
+                //将head的尾部地址指向n
+                n = n.next;
             }
+            //将n的next赋值新值，即head的尾部的next也赋值成了新值，完成添加
             n.next = new Node(data, null);
         }
         size++;
     }
 
-    public void insertTail(T data) {
-    }
-
-    public int size(){
+    public int size() {
         return this.size;
     }
 
@@ -41,21 +40,44 @@ public class MyLinkedList<T> {
                 && head.next != null;
     }
 
-    public void next(){
+
+    public void next() {
         head = head.next;
     }
 
     public void printList() {
         Node n = head;
-        if (hasNext()) {
-            System.out.println(head.data + "->" + head.next.data);
-            this.next();
+        while (n != null) {
+            System.out.print("->");
+            System.out.print(n.data);
+            n = n.next;
         }
+        System.out.println();
     }
 
 
+    public boolean palindrome() {
+        return false;
+    }
+
+    private Node revertNodeWithList(Node n){
+        List<T> list = new ArrayList<T>();
+        Node p = n;
+        while (p != null) {
+            list.add(p.data);
+            p = p.next;
+        }
+        Node result = null;
+        Node tail = null;
+        for (int i = list.size() - 1; i > 0; i--) {
+
+        }
+        return n;
+    }
+
     class Node {
         T data;
+
         Node next;
 
         public Node(T data, Node next) {
@@ -66,14 +88,10 @@ public class MyLinkedList<T> {
         public T getData() {
             return data;
         }
-
         public Node getNext() {
             return next;
         }
-    }
 
-    public boolean palindrome(){
-        return false;
     }
 
 
