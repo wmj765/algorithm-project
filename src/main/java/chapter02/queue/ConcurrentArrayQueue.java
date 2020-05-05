2b03752ac2656210b84fe5fed79a618dcdaf636c
maojia <Maojia.Wang@lingyue-digital.com>
2020-05-05 13:24:54 +0800

    finish concurrent array queue;

--- src/main/java/chapter02/queue/ConcurrentArrayQueue.java
+++ src/main/java/chapter02/queue/ConcurrentArrayQueue.java
@@ -1,6 +1,6 @@
 package chapter02.queue;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 并发安全的队列
@@ -10,9 +10,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ConcurrentArrayQueue {
 
     //头指针index
-    private AtomicInteger head;
+    private volatile int head;
     //尾指针index
-    private AtomicInteger tail;
+    private volatile int tail;
+
+    private final ReentrantLock lock;
     //数组大小
     private int capacity;
     //存放元素
@@ -26,6 +28,7 @@ public class ConcurrentArrayQueue {
     public ConcurrentArrayQueue(int capacity) {
         arr = new String[capacity];
         this.capacity = capacity;
+        lock = new ReentrantLock();
     }
 
     /**
@@ -35,59 +38,78 @@ public class ConcurrentArrayQueue {
      * @return 是否成功
      */
     public boolean enqueue(String value) {
-        int temp = tail.get() + 1;
-        for (; ; ) {
-            if (tail.compareAndSet(tail.get(), temp)) {
-                arr[tail.get()] = value;
-                tail.incrementAndGet();
-                break;
+        try {
+            lock.lock();
+            if (tail == capacity) {
+                return false;
             }
+            System.out.print("tail=" + tail + ",");
+            arr[tail] = value;
+            tail += 1;
+            return true;
+        } finally {
+            lock.unlock();
         }
-        return true;
     }
 
-//    public void printQueue() {
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = head; i < tail; i++) {
-//            sb.append(arr[i]);
-//        }
-//        System.out.println("队列中元素为" + sb.toString() + ",head=" + head + ",tail=" + tail);
-//    }
-//
-//    /**
-//     * 出队
-//     *
-//     * @return 队头的元素
-//     */
-//    public String dequeue() {
-//        if (head == tail) {
-//            throw new IndexOutOfBoundsException("队列中已没有元素");
-//        }
-//        String value = arr[head];
-//        head++;
-//        return value;
-//    }
+    public void printQueue() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = head; i < tail; i++) {
+            sb.append(arr[i]);
+        }
+        System.out.println("队列中元素为" + sb.toString() + ",head=" + head + ",tail=" + tail);
+    }
+
+    /**
+     * 出队
+     *
+     * @return 队头的元素
+     */
+    public String dequeue() {
+        try {
+            lock.lock();
+            if (head == tail) {
+                throw new IndexOutOfBoundsException("队列中已没有元素");
+            }
+            String value = arr[head];
+            head++;
+            System.out.print(value);
+            return value;
+        } finally {
+            lock.unlock();
+        }
+    }
 
     /**
      * 测试
      */
-    public static void main(String[] args) {
-        ConcurrentArrayQueue arrayQueue = new ConcurrentArrayQueue(10);
-        for (int i = 0; i < 10; i++) {
-            arrayQueue.enqueue(String.valueOf(i));
-            System.out.println(i + "入队");
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentArrayQueue arrayQueue = new ConcurrentArrayQueue(1000);
+        //20 个线程同时并发入队，tail 正常位移则说明线程安全
+        for (int i = 1; i < 20; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    arrayQueue.enqueue(String.valueOf(j));
+                }
+            }).start();
         }
-//        arrayQueue.printQueue();
-//
-//        for (int i = 0; i < 6; i++) {
-//            System.out.println(arrayQueue.dequeue() + "出队");
-//        }
-//        arrayQueue.printQueue();
-//
-//        for (int i = 0; i < 10; i++) {
-//            arrayQueue.enqueue(String.valueOf(i));
-//            System.out.println(i + "入队");
-//        }
-//        arrayQueue.printQueue();
+
+        //sleep 2s，让所有线程执行完成
+        Thread.sleep(2000);
+        //打印队列结果
+        arrayQueue.printQueue();
+
+        //出队，与入队顺序相同则线程安全
+        for (int i = 1; i < 20; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    //不能在此打印出队元素对比，因为在这没有线程保护不安全，为了对比将打印结果放到dequeue方法里面
+                    String dequeue = arrayQueue.dequeue();
+//                    System.out.println(dequeue);
+                }
+            }).start();
+        }
+
+
     }
 }
