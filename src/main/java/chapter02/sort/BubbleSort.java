10e45e61149ff52b7f60a36d1f019449e6fde826
maojia <Maojia.Wang@lingyue-digital.com>
2020-05-29 21:17:30 +0800

    add recursion

--- src/main/java/chapter02/sort/BubbleSort.java
+++ src/main/java/chapter02/sort/BubbleSort.java
@@ -8,12 +8,17 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr[] = {3, 5, 7, 8, 12, 1, 2, 10};
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            boolean sorted = true;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    sorted = false;
                 }
+            }
+            if (sorted) {
+                return;
             }
             printArr(arr);
             System.out.println();
