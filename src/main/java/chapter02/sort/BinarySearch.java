d72611a3387e40952842c75df1f9f438b2e6a9cf
maojia <Maojia.Wang@lingyue-digital.com>
2020-06-13 21:29:26 +0800

    search the first element which equals with target in arr

--- src/main/java/chapter02/sort/BinarySearch.java
+++ src/main/java/chapter02/sort/BinarySearch.java
@@ -7,26 +7,52 @@ package chapter02.sort;
  **/
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 4, 6, 7, 12, 45, 67, 78, 79, 80, 87, 89};
-        int target = 792;
-        System.out.println(binarySearch(arr, target));
+        int[] arr = {1, 1, 4, 6, 7, 7, 7, 7, 7, 7, 80, 87, 89};
+        int target = 1;
+        System.out.println(binarySearchFirstElement(arr, target));
     }
 
     /**
-     * 二分查找 注意终止条件为i<=j
+     * find the element equals with target
      *
-     * @param arr    数组
-     * @param target 目标值
-     * @return 数组位置
+     * @param arr    arr
+     * @param target target element
+     * @return index of the element which is equals with target
      */
-    public static int binarySearch(int[] arr, int target) {
+    public static int simpleBinarySearch(int[] arr, int target) {
         int i = 0;
         int j = arr.length - 1;
         while (i <= j) {
-            System.out.println(i + "," + j);
             int mid = i + (j - i) / 2;
             if (arr[mid] == target) {
                 return mid;
+            } else if (arr[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * find the first element equals with target
+     *
+     * @param arr    arr
+     * @param target target element
+     * @return first index of the element which is equals with target
+     */
+    public static int binarySearchFirstElement(int[] arr, int target) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (arr[mid] == target) {
+                if (mid != 0 && arr[mid - 1] == target) {//continue to find the element if it is not the first index
+                    j = mid - 1;
+                } else {
+                    return mid;
+                }
             } else if (arr[mid] < target) {
                 i = mid + 1;
             } else {
